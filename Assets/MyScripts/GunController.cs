1a8cb12465a0e00353eb8d21de46fba017d8b3dd
MeherBenSalem <mahourabensalem@gmail.com>
2023-02-14 20:14:52 +0100

    gun_sound_muzzle

--- Assets/MyScripts/GunController.cs
+++ Assets/MyScripts/GunController.cs
@@ -10,40 +10,43 @@ public int startingAmmo = 10;
 public float bulletSpeed = 100f;
 public Transform firePoint;
 private int currentAmmo;
+[SerializeField] GameObject muzzle;
+[SerializeField] AudioSource gunSound;
 private bool isAuto = false;
 private void Start() {
     currentAmmo = startingAmmo;
 }
 
 void Update() {
-    if (Input.GetButtonDown("Fire1") && currentAmmo > 0) { // Check if left mouse button is pressed and there is ammo remaining
+    if (Input.GetButtonDown("Fire1") && currentAmmo > 0) {
         Fire();
     }
 
-    if (isAuto && Input.GetButton("Fire1") && currentAmmo > 0) { // Check if gun is automatic and left mouse button is held down
+    if (isAuto && Input.GetButton("Fire1") && currentAmmo > 0) {
         Fire();
     }
 }
 
 void Fire() {
     currentAmmo--; // Decrement the current ammo count
-
-    Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f)); // Create a raycast from the center of the screen
+    GameObject muzzleOb = Instantiate(muzzle,firePoint.position,Quaternion.identity);
+    Destroy(muzzleOb,1f);
+    gunSound.Play();
+    Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
     RaycastHit hit;
-
-    if (Physics.Raycast(ray, out hit)) { // Cast the raycast and check if it hits anything
-        Debug.DrawLine(firePoint.position, hit.point, Color.red, 1f); // Draw a debug line from the fire point to the point of impact
+    if (Physics.Raycast(ray, out hit)) {
+        Debug.DrawLine(firePoint.position, hit.point, Color.red, 1f);
         // Add code here to apply damage to the hit object or perform other actions
     } else {
-        Debug.DrawLine(firePoint.position, ray.origin + (ray.direction * 100f), Color.red, 1f); // Draw a debug line from the fire point to the end of the raycast
+        Debug.DrawLine(firePoint.position, ray.origin + (ray.direction * 100f), Color.red, 1f);
     }
-    StartCoroutine(Reload()); // Start reloading if out of ammo
+    StartCoroutine(Reload());
 }
 
 IEnumerator Reload() {
     if (currentAmmo == 0) {
-        yield return new WaitForSeconds(1.5f); // Wait 1.5 seconds to reload
-        currentAmmo = maxAmmo; // Reset the ammo count to max capacity
+        yield return new WaitForSeconds(1.5f);
+        currentAmmo = maxAmmo;
     }
 }
 }
